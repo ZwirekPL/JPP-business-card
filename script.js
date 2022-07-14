e1f06e755afebf176a5070960d54b20582e78707
Zwirek_Dev <glanzgarage@gmail.com>
2022-07-14 16:19:50 +0200

    add mobile menu and form to change

--- script.js
+++ script.js
@@ -6,13 +6,12 @@
         })
    })
 
-
-//    $('.nav .menu li a').on('click', function () {
-//     const goToSection = "#" + $(this).attr('class');
-//     $('body, html').animate({
-//         scrollTop: $(goToSection).offset().top
-//     })
-// })
+   $('.mobileMenu .menuHam .navHam li a').on('click', function () {
+    const goToSection = "#" + $(this).attr('class');
+    $('body, html').animate({
+        scrollTop: $(goToSection).offset().top
+    })
+})
 
 
 
